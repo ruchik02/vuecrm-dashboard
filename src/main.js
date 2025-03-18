baafd4f63855de0d8f201815439df786c235153a
ruchika <ruchikasharma.cse19@chitkarauniversity.edu.in>
2025-03-18 12:54:08 +0530

    setup vue, vuex, vue-router, vuetify and icon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,25 @@
-import './assets/main.css'
+import { createVuetify } from "vuetify";
+import "vuetify/styles";
+import * as components from "vuetify/components";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router/router";
+import "@mdi/font/css/materialdesignicons.css";
+import store from "./store/store";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+const vuetify = createVuetify({
+  components,
+  icons: {
+    defaultSet: "mdi",
+  },
+  theme: {
+    defaultTheme: "light",
+    themes: {},
+  },
+});
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(vuetify);
+app.use(store);
+app.use(router);
+app.mount("#app");
---
+++ src/router/router.js
@@ -0,0 +1,14 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+// Import your components
+
+// Define routes
+const routes = [];
+
+// Create router instance
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
